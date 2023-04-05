ac8f48c70c2a05b66437ef8bec8d50500133c1f4
Adeel Rehman <adeel2001@live.com>
2023-04-05 18:28:42 -0500

    random ints

--- testgen.py
+++ testgen.py
@@ -1,14 +1,21 @@
+import random
 import struct
 
-# Generate pairs of 16 bit signed integers
+# Generate 10000 pairs of 16-bit signed integers
 pairs = []
 for i in range(10000):
-    x = struct.pack('h', i)
-    y = struct.pack('h', -i)
+    x = random.randint(-32768, 32767)
+    y = random.randint(-32768, 32767)
     pairs.append((x, y))
 
-# Write the pairs to a dat file
+# Pack the pairs into bytes objects
+packed_pairs = []
+for x, y in pairs:
+    packed_x = struct.pack('h', x)
+    packed_y = struct.pack('h', y)
+    packed_pairs.append(packed_x + packed_y)
+
+# Write the packed pairs to a dat file
 with open('pairs.dat', 'wb') as f:
-    for x, y in pairs:
-        f.write(x)
-        f.write(y)
+    for packed_pair in packed_pairs:
+        f.write(packed_pair)
